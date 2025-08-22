7235758d94a3c29856409e14d3284277a6230a29
Caedon <caedon@MacBookAir.lan>
2025-08-22 02:01:18 -0500

    added tailwind styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,15 @@ import Card from './Components/Card/Card';
 import './App.css'
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/search';
+import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import type { CompanySearch } from './company';
 import { searchCompanies } from './api';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   const[search,setSearch] = useState<string>("");     // Creating a memory box called search that starts empty
+  const[portfolioValues, setPortfolioValues] = useState<string[]>([]); // Creating an array called portfolioValues that starts empty
   const[searchResults, setSearchResults] = useState<CompanySearch[]>([]); // Creating a memory box called searchResults that starts empty
   const[serverError, setServerError] = useState<string>(""); // Creating a memory box called serverError that starts empty
 
@@ -17,11 +21,23 @@ function App() {
         setSearch(e.target.value);  // getting the value from the input field
         console.log(e);
     };
-    const onPortfolioCreate = (e: SyntheticEvent) => {
+    const onPortfolioCreate = (e: any) => {
       e.preventDefault(); // Prevents the default form submission behavior
-      console.log();
+      const exists = portfolioValues.find((value) => value === e.target[0].value); // Prevents adding duplicates
+      if(exists) return;
+      const updatedPortfolio= [...portfolioValues, e.target[0].value]; // Adds the current search value to the portfolio values
+      setPortfolioValues(updatedPortfolio); // Updates the portfolio values state
     }
 
+    // Handles deleteing a portfolio item
+    const onPortfolioDelete = (e: any)=>
+      {
+        e.preventDefault(); // Prevents the default form submission behavior
+        const removed = portfolioValues.filter((value) =>{
+          return value !== e.target[0].value;
+        });
+        setPortfolioValues(removed); // Updates the portfolio values state with the removed item
+      }
     // Listens for when someone clicks the search button
     // When they click, it logs the event to the console
     // This is where you would add functionality to perform a search
@@ -37,9 +53,14 @@ function App() {
       }
   return (
     <div className="App">
+        <Navbar />
+        
         <Search onSearchSubmit={onSearchSubmit} search={search}  handleSearchChange={handleSearchChange}/>
+        <ListPortfolio 
+        portfolioValues={portfolioValues}
+        onPortfolioDelete={onPortfolioDelete}/>
         <CardList searchResults={searchResults} onPortfolioCreate={onPortfolioCreate} />
-        {serverError && <div>Unable to connect to API</div>}
+        {serverError && <div className="text-red-500 p-4">Unable to connect to API</div>}
 
     </div>
   );
---
+++ frontend/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -0,0 +1,22 @@
+import React, { type SyntheticEvent } from "react";
+
+interface Props {
+
+    onPortfolioDelete: (e: SyntheticEvent) => void; // Function to handle portfolio deletion
+    portfolioValue: string; // single string
+}
+
+const DeletePortfolio = ({onPortfolioDelete, portfolioValue}: Props) => {
+    return <div>
+        <form onSubmit={onPortfolioDelete}>
+        <input hidden={true} name="portfolioValue" value={portfolioValue} />
+         <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
+          X
+        </button>
+            
+        </form>
+
+    </div>
+}
+
+export default DeletePortfolio;
